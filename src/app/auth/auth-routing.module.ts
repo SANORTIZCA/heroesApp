7a7cf7b4e5f6b9a2a025b65d076610cb04b7f4e0
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-07-28 17:34:33 -0500

    Se crea la estructura de rutas con lazyload para la app

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+
+const routes: Routes = [
+  {
+    //localhost:4200/auth/
+    path: '',
+    component: LayoutPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: LayoutPageComponent,
+    /* loadchildren para lazyload */
+    // Como este modulo ya es cargado con lazyload, entonces no es necesario cargas las hijas con lazyload, a menos de que sea un módulo muy grande
+    children: [
+      {
+        path: 'new-hero',
+        component: NewPageComponent
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
