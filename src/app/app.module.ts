c3b9cb8d2418264c4347176e4c394f967b72be2c
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-09-27 19:07:53 -0500

    Se crea un servicio para consumir el endpoint de la lista de heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
@@ -14,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     SharedModule,
   ],
   providers: [
