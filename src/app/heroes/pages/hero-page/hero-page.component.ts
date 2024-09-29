e37193112091da3c292df5b31b14425a1426f262
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-09-29 18:16:02 -0500

    Se crea un servicio para traer los heroes por URL y se maneja el error si regresa un heroe que no existe y saca a la persona de la vista, en caso contrario, se establece el heroe

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { switchMap } from 'rxjs';
+
+import { HeroesService } from './../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
+
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    /* servicio propio del router */
+    private activedRoute: ActivatedRoute,
+    /* Necesario para sacar a la persona de la vista */
+    private router: Router,
+
+  ){}
+
+  ngOnInit(): void {
+    this.activedRoute.params
+      .pipe(
+        /* swithcMap permite tomar de los params el valor desestructurado */
+        switchMap(({id})=> this.heroesService.getHeroById(id)),
+      )
+      .subscribe( hero => {
+        /* saca a la persona de la vista */
+        if(!hero) return this.router.navigate(['/heroes/list']);
+        this.hero=hero;
+        console.log({hero});
+        return;
+      })
+  }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 import { Hero } from '../interfaces/hero.interface';
 
@@ -18,7 +18,15 @@ export class HeroesService {
   constructor(private http: HttpClient) { }
 
   getHeroes():Observable<Hero[]>{
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  getHeroById(id:string): Observable<Hero|undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        /* of de rxjs, es una forma de crear un observable basado en el valor que se pasa en el parentesis */
+        catchError( error => of(undefined))
+      )
 
   }
 }
