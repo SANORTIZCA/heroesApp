6355c6541cdb7d1961aaadd2ae978ca3a44a44c7
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-09-29 20:00:27 -0500

    Se crea la estructura en HTML para editar los heroes que se utilizara en la siguiente sección en donde se utilizan formularios reactivos

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NewPageComponent {
-
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics'},
+    { id: 'Marvel Comics', desc: 'Marvel - Comics'},
+  ];
 }
